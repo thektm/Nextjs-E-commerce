86b0bb7d79179a480d99c2dd6e99cd68df82e482
thektm <138784494+thektm@users.noreply.github.com>
2025-04-03 03:24:43 +0330

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,23 +6,26 @@ import { CartProvider } from "@/context/CartContext";
 import { AuthProvider } from "@/context/AuthContext";
 import { SelectionProvider } from "@/context/SelectionCOntext";
 import ToastContainerComponent from "@/componenets/ToastContainerComponent";
-import  AboutUs  from "@/componenets/AboutUs";
+import AboutUs from "@/componenets/AboutUs";
+import { ReactLenis } from "lenis/react";
 
 export default function Home() {
   return (
     <main className="">
-      <AuthProvider>
-        <SelectionProvider>
-          <CartProvider>
-            <ToastContainerComponent />
-            <Navbar />
-            <NewHero />
-            <Cataloge />
-            <AboutUs />
-            <Footer />
-          </CartProvider>
-        </SelectionProvider>
-      </AuthProvider>
+      <ReactLenis root>
+        <AuthProvider>
+          <SelectionProvider>
+            <CartProvider>
+              <ToastContainerComponent />
+              <Navbar />
+              <NewHero />
+              <Cataloge />
+              <AboutUs />
+              <Footer />
+            </CartProvider>
+          </SelectionProvider>
+        </AuthProvider>
+      </ReactLenis>
     </main>
   );
 }
